72d324d720d4e1412593550c106849f16eeafaee
cmora-1 <mora_corrupto@hotmail.com>
2015-05-09 13:06:38 +0200

    Crear pregunta

--- routes/index.js
+++ routes/index.js
@@ -8,12 +8,14 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
-// Autoload de comandos con :quizId
+// Autoload de comandos con :quizId  
 router.param('quizId', quizController.load);  // autoload :quizId
 
 // Definición de rutas de /quizes
 router.get('/quizes',                      quizController.index);
 router.get('/quizes/:quizId(\\d+)',        quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
+router.get('/quizes/new',                  quizController.new);
+router.post('/quizes/create',              quizController.create);
 
 module.exports = router;
